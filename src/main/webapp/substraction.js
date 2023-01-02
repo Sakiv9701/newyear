2c83e61d6c9ad8f407ad8d6884ec106d5ab4a876
Sakiv9701 <sonukumar012397@gmail.com>
2023-01-02 10:18:45 +0530

    Added all the webApp files

---
+++ src/main/webapp/substraction.js
@@ -0,0 +1,9 @@
+function subtract() {
+var a = document.getElementById("firstno").value
+var b = document.getElementById("secondno").value
+var result
+result = a-b
+document.getElementById("result").value=result
+}
+Footer
+© 2022 GitHub, Inc.
